187d2f6a92163be259451316ab3e5ed9ee5745f7
malres <fuhongwei@doupai.cc>
2020-07-23 15:55:06 +0800

    7.23 对于native-lib中的缓冲区的理解

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -47,14 +47,17 @@ enum {
     kMsgDecodeDone,
     kMsgSeek,
 };
+
 //mylooper继承自looper
 //virtual：定义虚函数的关键字
 class mylooper: public looper{
     virtual void handle(int what, void* obj);
 };
-
+//static在此的作用是静态变量，*是表明mlooper是指针类型
 static mylooper *mlooper = NULL;
 
+//timespec：纳秒，来源于time.h
+//CLOCK_MONOTONIC：从系统启动这一刻起开始计时,不受系统时间被用户改变的影响
 __int64_t systemnanotime(){
     timespec now;
     clock_gettime(CLOCK_MONOTONIC, &now);
@@ -63,29 +66,45 @@ __int64_t systemnanotime(){
 
 void doCodecWork(workerdata *d){
 
+    /**
+     * ssize_t：有符号整形，在32位机器上等同于int,z 64位机器上等同于long int.
+     * queueInputBuffer和dequeueInputBuffer是一对方法，
+     * 应用场景是用于对输入的数据流进行编码或者解码处理的时候，通过各种方法获得一个ByteBuffer的数组,
+     * 然后调用dequeueInputBuffer方法提取出要处理的部分。处理完毕之后通过dequeueInputBuffer把ByteBuffer
+     * 放回队列中，就能释放内存。
+     * dequeueInputBuffer表示等待的时间（毫秒）
+     * size_t：有符号整形，表示操作数据块的大小
+     * */
     ssize_t bufidx = -1;
     if(!d->sawInputEOS){
+        //获取缓冲区，设置超时为2000毫秒
         bufidx = AMediaCodec_dequeueInputBuffer(d->codec, 2000);
         LOGV("input buffer %zd", bufidx);
         if(bufidx >= 0){
             size_t bufsize;
+            //取到缓冲区输入流
             auto buf = AMediaCodec_getInputBuffer(d->codec, bufidx, &bufsize);
+            //开始读取样本,d->ex已经设置了选中的轨道
             auto sampleSize = AMediaExtractor_readSampleData(d->ex, buf ,bufsize);
             if(sampleSize < 0){
                 sampleSize = 0;
                 d->sawInputEOS = true;
                 LOGV("EOS");
             }
+            //以微秒为单位返回当前样本的呈现时间
             auto presentationTimeUs = AMediaExtractor_getSampleTime(d->ex);
 
+            //将缓冲区传递至解码器
             AMediaCodec_queueInputBuffer(d->codec, bufidx, 0 ,sampleSize, presentationTimeUs,
                     d->sawInputEOS ? AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM : 0);
+            //前进到下一个样本
             AMediaExtractor_advance(d->ex);
         }
     }
 
     if(!d->sawOutputEOS){
         AMediaCodecBufferInfo info;
+        //缓冲区第一步
         auto status = AMediaCodec_dequeueOutputBuffer(d->codec, &info, 0);
         if(status >= 0){
             if(info.flags & AMEDIACODEC_BUFFER_FLAG_END_OF_STREAM){
@@ -99,8 +118,10 @@ void doCodecWork(workerdata *d){
             }
             int64_t delay = (d->renderstart + presentationNano) - systemnanotime();
             if(delay > 0){
+                //延时操作，如果缓冲区里的可展示时间>当前视频播放的进度，就休眠一下
                 usleep(delay/1000);
             }
+            //渲染，如果info.size != 0等于true，就会渲染到surface上第二步
             AMediaCodec_releaseOutputBuffer(d->codec, status, info.size != 0);
             if(d->renderonce){
                 d->renderonce = false;
@@ -113,6 +134,7 @@ void doCodecWork(workerdata *d){
             LOGV("format changed to: %s", AMediaFormat_toString(format));
             AMediaFormat_delete(format);
         } else if (status == AMEDIACODEC_INFO_TRY_AGAIN_LATER){
+            //解码当前帧超时
             LOGV("no output buffer right now");
         } else{
             LOGV("unexpected info code : %zd", status);
@@ -120,10 +142,12 @@ void doCodecWork(workerdata *d){
     }
 
     if(!d->sawInputEOS || !d->sawOutputEOS){
+        //如果输入或者输出没有结束，就回调自己
         mlooper->post(kMsgCodecBuffer, d);
     }
 }
 
+//此处重写了消息的处理方法
 void mylooper::handle(int what, void *obj) {
     switch (what){
         case kMsgCodecBuffer:
@@ -157,7 +181,7 @@ void mylooper::handle(int what, void *obj) {
             workerdata *d = (workerdata*)obj;
             if (d->isPlaying){
                 d->isPlaying = false;
-                post(kMsgPauseAck, NULL, true);
+                post(kMsgPauseAck, NULL, true);//清空队列
             }
         }break;
 
@@ -186,7 +210,7 @@ extern "C"{
 
         off_t outStart, outLen;
         int fd = AAsset_openFileDescriptor(AAssetManager_open(AAssetManager_fromJava(env, assetMgr), utf8, 0),
-                                           &outStart, &outLen);
+                                           &outStart, &outLen);//打开视频文件
 
         env->ReleaseStringUTFChars(filename, utf8);
         if (fd < 0){
@@ -196,6 +220,7 @@ extern "C"{
 
         data.fd = fd;
 
+        //用于保存当前播放用到的一些标志位
         workerdata *d = &data;
 
         AMediaExtractor *ex = AMediaExtractor_new();
@@ -209,8 +234,10 @@ extern "C"{
             return JNI_FALSE;
         }
 
+        //获取轨道数
         int numtracks = AMediaExtractor_getTrackCount(ex);
 
+        //负责媒体文件的编码和解码工作
         AMediaCodec *codec = NULL;
 
         LOGV("input has %d tracks", numtracks);
@@ -227,15 +254,15 @@ extern "C"{
                 //生产代码应检查错误
                 AMediaExtractor_selectTrack(ex, i);
                 codec = AMediaCodec_createDecoderByType(mime);
-                AMediaCodec_configure(codec, format, d->window, NULL, 0);
-                d->ex = ex;
-                d->codec = codec;
-                d->renderstart = -1;
+                AMediaCodec_configure(codec, format, d->window, NULL, 0);//d->window 与解码器绑定
+                d->ex = ex;//视频轨道
+                d->codec = codec;//解码器
+                d->renderstart = -1;//开始渲染时间
                 d->sawInputEOS = false;
                 d->sawOutputEOS = false;
                 d->isPlaying = false;
                 d->renderonce = true;
-                AMediaCodec_start(codec);
+                AMediaCodec_start(codec);//开始解码
             }
             //MediaFormat 最小的音频格式
             AMediaFormat_delete(format);
